f9a401ff4072a00a462ca43e5e2775bcbd6a497a
lingwang <lingwang@xiaoe-tech.com>
2021-03-15 14:54:49 +0800

    init

---
+++ FileInAndFileOut/src/main/java/com/example/FileInAndFileOut/FileInAndFileOutApplication.java
@@ -0,0 +1,13 @@
+package com.example.FileInAndFileOut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileInAndFileOutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileInAndFileOutApplication.class, args);
+	}
+
+}
---
+++ FileInAndFileOut/src/test/java/com/example/FileInAndFileOut/FileInAndFileOutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FileInAndFileOut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileInAndFileOutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
