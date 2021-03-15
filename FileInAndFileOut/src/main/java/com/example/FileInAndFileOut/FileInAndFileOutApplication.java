89ed137bf14cc2133fe09ac3e19bdb200b63c1ca
lingwang <lingwang@xiaoe-tech.com>
2021-03-15 17:16:58 +0800

    获取别人服务

--- FileInAndFileOut/src/main/java/com/example/FileInAndFileOut/FileInAndFileOutApplication.java
+++ FileInAndFileOut/src/main/java/com/example/FileInAndFileOut/FileInAndFileOutApplication.java
@@ -1,5 +1,6 @@
 package com.example.FileInAndFileOut;
 
+import com.fasterxml.jackson.datatype.jsr310.ser.YearSerializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ public class FileInAndFileOutApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FileInAndFileOutApplication.class, args);
+		System.out.println("启动成功");
 	}
 
 }
